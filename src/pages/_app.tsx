f837eecc34b67e0042d958936be8a8986209b7bc
Greg <gskril@gmail.com>
2023-03-22 17:39:38 -0400

    Set up Google fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,33 @@ import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import '@rainbow-me/rainbowkit/styles.css'
 import PlausibleProvider from 'next-plausible'
 import type { AppProps } from 'next/app'
+import { Public_Sans, Urbanist } from 'next/font/google'
 import { WagmiConfig } from 'wagmi'
 
 import { chains, wagmiClient } from '@/providers'
 import '@/styles/style.scss'
 
+const urbanist = Urbanist({ subsets: ['latin'] })
+const publicSans = Public_Sans({ subsets: ['latin'] })
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PlausibleProvider domain="example.com" trackOutboundLinks>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider chains={chains} modalSize="compact">
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </PlausibleProvider>
+    <>
+      <style jsx global>{`
+        :root {
+          --font-fallback: system-ui, 'Helvetica Neue', sans-serif;
+          --font-heading: ${urbanist.style.fontFamily}, var(--font-fallback);
+          --font-body: ${publicSans.style.fontFamily}, var(--font-fallback);
+        }
+      `}</style>
+
+      <PlausibleProvider domain="example.com" trackOutboundLinks>
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider chains={chains} modalSize="compact">
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </PlausibleProvider>
+    </>
   )
 }
