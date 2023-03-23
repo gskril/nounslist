17ff5ea61e83df18978c8cc723058cbaf7df92e8
Greg <gskril@gmail.com>
2023-03-23 02:19:03 -0400

    Revert "Use public provider"
    This reverts commit e97a97428c6b815a7675e4b4423cbc66c73601e0.

--- src/providers.ts
+++ src/providers.ts
@@ -1,11 +1,14 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { configureChains, createClient } from 'wagmi'
 import { goerli, mainnet } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
+
+const ALCHEMY_ID = process.env.NEXT_PUBLIC_ALCHEMY_ID || ''
 
 export const { chains, provider } = configureChains(
   [mainnet, goerli],
-  [publicProvider()]
+  [alchemyProvider({ apiKey: ALCHEMY_ID }), publicProvider()]
 )
 
 const { connectors } = getDefaultWallets({
