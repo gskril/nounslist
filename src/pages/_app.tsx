09fee0993a73a9a46aa2990c7744ed1ec670ed99
Greg <gskril@gmail.com>
2023-04-03 17:07:01 -0400

    Add plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
         }
       `}</style>
 
-      <PlausibleProvider domain="example.com" trackOutboundLinks>
+      <PlausibleProvider domain="nounslist.com" trackOutboundLinks>
         <WagmiConfig client={wagmiClient}>
           <RainbowKitProvider chains={chains} modalSize="compact">
             <Component {...pageProps} />
